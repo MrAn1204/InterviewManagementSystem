1bee7d6226d59bef1b84ee66373ed0f0156196e5
Loc179 <locnt179@gmail.com>
2025-03-18 21:19:57 +0700

    Create form forget-password

--- src/client/src/app/modules/auth/login/login.component.ts
+++ src/client/src/app/modules/auth/login/login.component.ts
@@ -7,7 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor() {
-    console.log('LoginComponent loaded!');
-  }
+
 }
