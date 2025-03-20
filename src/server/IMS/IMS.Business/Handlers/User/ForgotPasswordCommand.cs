00331ac8887009e8ada4e9fce1f3923eb27b9d62
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-03-20 16:10:53 +0700

    Move handler outside

--- src/server/IMS/IMS.Business/Handlers/User/ForgotPasswordCommand.cs
+++ src/server/IMS/IMS.Business/Handlers/User/ForgotPasswordCommand.cs
@@ -1,5 +1,4 @@
 using System;
-using IMS.Business.Services;
 using MediatR;
 
 namespace IMS.Business.Handlers.User;
@@ -7,28 +6,4 @@ namespace IMS.Business.Handlers.User;
 public class ForgotPasswordCommand : IRequest
 {
     public required string Email { get; set; }
-}
-
-public class ForgotPasswordCommandHandler(IEmailService emailService) : IRequestHandler<ForgotPasswordCommand>
-{
-    private readonly IEmailService _emailService = emailService;
-
-    public async Task Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
-    {
-        string subject = "Password Reset";
-        string message = $@"
-            <html>
-            <body>
-                <p>We have just received a password reset request for <b>{request.Email}</b>.</p>
-                <p>Please click <a href='http://localhost:4200/reset-password' style='color: blue; text-decoration: underline;'>here</a> to reset your password.</p>
-                <p>For your security, the link will expire in <b>24 hours</b> or immediately after you reset your password.</p>
-                <br>
-                <p>Thanks & Regards!<br>
-                <b>IMS Team</b></p>
-            </body>
-            </html>
-        ";
-
-        await _emailService.SendEmailAsync(request.Email, subject, message);
-    }
 }
