e3ba3534d09cc4148e7487d9a871785b14dc6f20
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-03-24 21:26:17 +0700

    Move command handler

--- src/server/IMS/IMS.Business/Handlers/Auth/ValidateResetPasswordCommand.cs
+++ src/server/IMS/IMS.Business/Handlers/Auth/ValidateResetPasswordCommand.cs
@@ -1,5 +1,4 @@
 using System;
-using IMS.Business.Services;
 using MediatR;
 
 namespace IMS.Business.Handlers;
@@ -7,19 +6,4 @@ namespace IMS.Business.Handlers;
 public class ValidateResetPasswordCommand : IRequest<bool>
 {
     public required string Token { get; set; }
-}
-
-
-public class ValidateResetPasswordCommandHandler(
-    ITokenService tokenService
-) : IRequestHandler<ValidateResetPasswordCommand, bool>
-{
-    private readonly ITokenService _tokenService = tokenService;
-
-    public async Task<bool> Handle(ValidateResetPasswordCommand request, CancellationToken cancellationToken)
-    {
-        var result = await _tokenService.ValidateResetPasswordAsync(request.Token);
-        
-        return result;
-    }
 }
